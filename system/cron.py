531e09ce167c44387fc9c6505dd0453a7251b8f9
Gregor Giesen <giesen@zaehlwerk.net>
2016-09-10 13:19:55 +0200

    cron: replacement for os.getlogin()
    os.getlogin() returns the user logged in on the controlling terminal. However
    'crontab' only looks for the login name of the process' real user id which
    pwd.getpwuid(os.getuid())[0] does provide.
    
    While in most cases there is no difference, the former might fail under certain
    circumstances (e.g. a lxc container connected by attachment without login),
    throwing the error 'OSError: [Errno 25] Inappropriate ioctl for device'.

--- system/cron.py
+++ system/cron.py
@@ -198,6 +198,7 @@ EXAMPLES = '''
 '''
 
 import os
+import pwd
 import re
 import tempfile
 import platform
@@ -477,7 +478,7 @@ class CronTab(object):
                 return "%s -l %s" % (pipes.quote(CRONCMD), pipes.quote(self.user))
             elif platform.system() == 'HP-UX':
                 return "%s %s %s" % (CRONCMD , '-l', pipes.quote(self.user))
-            elif os.getlogin() != self.user:
+            elif pwd.getpwuid(os.getuid())[0] != self.user:
                 user = '-u %s' % pipes.quote(self.user)
         return "%s %s %s" % (CRONCMD , user, '-l')
 
@@ -489,7 +490,7 @@ class CronTab(object):
         if self.user:
             if platform.system() in ['SunOS', 'HP-UX', 'AIX']:
                 return "chown %s %s ; su '%s' -c '%s %s'" % (pipes.quote(self.user), pipes.quote(path), pipes.quote(self.user), CRONCMD, pipes.quote(path))
-            elif os.getlogin() != self.user:
+            elif pwd.getpwuid(os.getuid())[0] != self.user:
                 user = '-u %s' % pipes.quote(self.user)
         return "%s %s %s" % (CRONCMD , user, pipes.quote(path))
 

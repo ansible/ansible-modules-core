791d634085eb27d88648de8ed314925fb2e297e2
John R Barker <john@johnrbarker.com>
2016-10-21 16:13:57 +0100

    Run versioned validate-module from ansible/ansible stable-2.2 (#5330)
    * Remove docs for commit which no longer exists (#5152)
    
    (cherry picked from commit c5b2dafa59da3dd2acc8ca042c8d87bd0dd99385)
    
    * Run validate-modules from ansible/ansible stable-2.2
    
    * submodules need updating

--- test/utils/shippable/sanity.sh
+++ test/utils/shippable/sanity.sh
@@ -6,16 +6,30 @@ install_deps="${INSTALL_DEPS:-}"
 
 cd "${source_root}"
 
-if [ "${install_deps}" != "" ]; then
-    add-apt-repository ppa:fkrull/deadsnakes && apt-get update -qq && apt-get install python2.4 -qq
+build_dir=$(mktemp -d)
+trap 'rm -rf "${build_dir}"' EXIT
 
+git clone "https://github.com/ansible/ansible.git" "${build_dir}" --recursive
+cd "${build_dir}"
+git checkout stable-2.2
+git submodule update --init
+cd "${source_root}"
+source "${build_dir}/hacking/env-setup"
+
+if [ "${install_deps}" != "" ]; then
+    add-apt-repository ppa:fkrull/deadsnakes
     apt-add-repository 'deb http://archive.ubuntu.com/ubuntu trusty-backports universe'
     apt-get update -qq
-    apt-get install shellcheck
 
-    pip install git+https://github.com/ansible/ansible.git@stable-2.2#egg=ansible
-    pip install git+https://github.com/sivel/ansible-testing.git#egg=ansible_testing
+    apt-get install -qq shellcheck python2.4
+
+    # Install dependencies for ansible and validate_modules
+    pip install -r "${build_dir}/test/utils/shippable/sanity-requirements.txt" --upgrade
+    pip list
+
 fi
+
+validate_modules="${build_dir}/test/sanity/validate-modules/validate-modules"
 
 python2.4 -m compileall -fq   -i                    "test/utils/shippable/sanity-test-python24.txt"
 python2.4 -m compileall -fq   -x "($(printf %s "$(< "test/utils/shippable/sanity-skip-python24.txt"))" | tr '\n' '|')" .
@@ -24,7 +38,7 @@ python2.7 -m compileall -fq .
 python3.5 -m compileall -fq . -x "($(printf %s "$(< "test/utils/shippable/sanity-skip-python3.txt"))"  | tr '\n' '|')"
 
 ANSIBLE_DEPRECATION_WARNINGS=false \
-    ansible-validate-modules --exclude '/utilities/|/shippable(/|$)' .
+    "${validate_modules}" --exclude '/utilities/|/shippable(/|$)' .
 
 shellcheck \
     test/utils/shippable/*.sh

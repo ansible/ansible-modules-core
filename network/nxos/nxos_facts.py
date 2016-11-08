50fe7fb10a763f62884f29f3ef893c14049f2dda
Jason Edelman <jedelman8@gmail.com>
2016-11-07 17:50:14 -0800

    fix parsing show module command for 9372 (#5485)

--- network/nxos/nxos_facts.py
+++ network/nxos/nxos_facts.py
@@ -428,6 +428,8 @@ class Legacy(FactsBase):
 
     def parse_module(self, data):
         data = data['TABLE_modinfo']['ROW_modinfo']
+        if isinstance(data, dict):
+            data = [data]
         objects = list(self.transform_iterable(data, self.MODULE_MAP))
         return objects
 
